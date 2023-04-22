ef7e28c047a6b3237592d150af7f97fb7beb47e8
nguyenvukhang <brew4k@gmail.com>
2023-04-22 12:53:57 +0800

    implement cli printing with borders

--- Aquarium/Engine/Sources/Cli/main.swift
+++ Aquarium/Engine/Sources/Cli/main.swift
@@ -3,26 +3,29 @@ import Foundation
 
 let raw_json = """
 {
-  "id": "MDo2LDA3MCw1NzI=",
+  "id": "MDo4LDM0MCw5KTA=",
   "size": 6,
   "sums": {
-    "cols": [2, 4, 5, 5, 4, 2],
-    "rows": [4, 3, 4, 4, 2, 5]
+    "cols": [5, 5, 5, 3, 3, 4],
+    "rows": [4, 4, 5, 5, 2, 5]
   },
   "matrix": [
-    [1, 2, 2, 2, 2, 3],
-    [1, 1, 2, 4, 2, 3],
-    [1, 2, 2, 4, 4, 3],
-    [1, 2, 2, 2, 2, 3],
-    [1, 5, 5, 5, 6, 3],
-    [1, 1, 1, 5, 6, 3]
+    [1, 1, 2, 2, 2, 2],
+    [1, 3, 3, 4, 4, 2],
+    [1, 3, 3, 4, 2, 2],
+    [5, 4, 4, 4, 6, 2],
+    [5, 4, 6, 6, 6, 6],
+    [5, 5, 5, 5, 5, 6]
   ],
 }
 """
+// "play": "https://www.puzzle-aquarium.com/?e=MDo4LDM0MCw5KTA="
 
 print("--- start Engine CLI ---")
 var board = try Board(withJson: raw_json)
+let n = board.size
+(0 ..< n).forEach { i in board.mat[n - 1][i] = .water }
 print(board)
-board = try Board(withProblemId: "MDo4LDM0MCw5OTA=")
-print(board)
+// board = try Board(withProblemId: "MDo4LDM0MCw5OTA=")
+// print(board)
 print(" --- end Engine CLI ---")
---
+++ Aquarium/Engine/Tests/borders.swift
@@ -0,0 +1,8 @@
+import Engine
+
+func border_tests() {
+    describe("1x1") {
+        var board = Board.empty(size: 1)
+        // assertEq(received: Equatable, expected: Equatable)
+    }
+}
