8b5709c9d6f5260deeabf9a8e5eba4bee9b9359e
nguyenvukhang <brew4k@gmail.com>
2023-04-29 15:49:33 +0800

    time break

--- Aquarium/Engine/Tests/SolveTests.swift
+++ Aquarium/Engine/Tests/SolveTests.swift
@@ -64,4 +64,30 @@ final class SolveTests: XCTestCase {
             [a, w, w, w, w, w],
         ])
     }
+
+    // This test takes way too long to run
+    // func test3() throws {
+    //     // https://www.puzzle-aquarium.com/?e=MjoxNSw4MzUsODU4
+    //     let board = Board(
+    //         colSums: [5, 4, 2, 1, 3, 5],
+    //         rowSums: [1, 5, 3, 4, 3, 4],
+    //         groups: [
+    //             [1, 1, 2, 3, 3, 4],
+    //             [5, 5, 2, 6, 6, 4],
+    //             [7, 8, 9, 9, 10, 10],
+    //             [11, 8, 12, 12, 13, 13],
+    //             [11, 14, 14, 12, 12, 15],
+    //             [16, 16, 17, 18, 18, 15],
+    //         ]
+    //     )
+    //
+    //     assertEq(board.solve(), [
+    //         [w, a, a, a, a, a],
+    //         [w, a, a, a, a, w],
+    //         [a, a, a, a, a, w],
+    //         [w, w, w, w, w, a],
+    //         [a, w, w, w, w, w],
+    //         [a, w, w, w, w, w],
+    //     ])
+    // }
 }
