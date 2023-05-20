6554027fa0c685b669d29a6b4011845cb685eac2
nguyenvukhang <brew4k@gmail.com>
2023-05-20 23:16:20 +0800

    make readable configs

--- Aquarium/Engine/Sources/EngineCli/main.swift
+++ Aquarium/Engine/Sources/EngineCli/main.swift
@@ -1,8 +1,27 @@
 import Engine
 
+// one-off enums
+enum Size: Int { case six = 6; case ten = 10; case fifteen = 15 }
+enum Difficulty: String { case easy; case normal; case hard }
+
+/**
+ * SET CONFIGS HERE
+ */
+let size: Size = .ten
+let difficulty: Difficulty = .hard
+let number = 6 // a number from 1 to 6 (inclusive)
+
+/**
+ * Using the really clutch config above, this program will look into
+ * the ../../problems/problem-db folder for a pre-downloaded problem
+ * to solve.
+ */
+let (s, d) = (size.rawValue, difficulty.rawValue)
+let filename = "../../problems/problem-db/\(s)x\(s)_\(d)_v\(number).json"
+
 print("--- START EngineCli ---\n")
 
-let game = try Game(withJsonFile: "../../problems/problem-db/6x6_hard_v3.json")
+let game = try Game(withJsonFile: filename)
 game.solve()
 
 print("\n--- END EngineCli ---")
